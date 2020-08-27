1f5781dad685fdffdb2149025006bb6ed7c231d7
Ang Cheng Jun <e0415830@u.nus..edu>
2020-08-27 15:38:58 +0800

    Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,10 +7,23 @@ public class Duke {
 
         Scanner sc = new Scanner(System.in);
         String command = sc.nextLine();
+        String[] list = new String[100];
         String exit = new String("bye");
+        String display = new String("list");
+        int num = 0;
 
         while(exit.equals(command) == false) {
-            System.out.println(command + "\n");
+            if(display.equals(command) == true) {
+                for (int i = 1; i <= num; i++) {
+                    System.out.println(i + "." + list[i - 1]);
+                }
+            }
+            else {
+                list[num] = command;
+                System.out.println("added: " + command);
+                num++;
+            }
+            System.out.println("\n");
             command = sc.nextLine();
         }
 
