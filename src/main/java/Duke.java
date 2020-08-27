52918f8665439e3fccdc6e0d0a228d6fe9fab0b9
Ang Cheng Jun <e0415830@u.nus..edu>
2020-08-27 15:36:48 +0800

    Level 1. Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?\n");
-        System.out.println("Bye. Hope to see you again soon!\n");
+
+        Scanner sc = new Scanner(System.in);
+        String command = sc.nextLine();
+        String exit = new String("bye");
+
+        while(exit.equals(command) == false) {
+            System.out.println(command + "\n");
+            command = sc.nextLine();
+        }
+
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
